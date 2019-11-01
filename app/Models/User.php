eab057575c722f1dde8168d61084d56819250c48
zr <1085530400@qq.com>
2019-11-01 15:14:44 +0800

    '邮箱验证'

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,25 @@
 <?php
 
-
 namespace App\Models;
 
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-use Illuminate\Database\Eloquent\Model;
-
-class User extends Model
+class User extends Authenticatable implements  MustVerifyEmail
 {
+    use Notifiable,MustVerifyEmailTrait;
 
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
