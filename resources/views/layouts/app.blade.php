6c32180dd23b20c3d1f601e3f883e2b951e6ef75
zrr <1085530400@qq.com>
2019-11-10 00:12:04 +0800

    '编辑器优化,WYSIWYG编辑器'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css')}}">
+    @yield('styles')
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
@@ -19,5 +20,6 @@
     @include('layouts._footer')
 </div>
 <script src="{{mix('js/app.js')}}"></script>
+@yield('scripts')
 </body>
 </html>
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -25,7 +25,7 @@
                                 <input type="hidden" name="_token" value="{{csrf_token()}}">
                                 @include('shared._error')
                                 <div class="form-group">
-                                    <input class="form-control" type="text" name="title" value="{{old('title',$topic->title)}}">
+                                    <input class="form-control" type="text" name="title" value="{{old('title',$topic->title)}}" placeholder="请填写标题">
                                 </div>
                                 <div class="form-group">
                                     <select class="form-control" name="category_id" required id="">
@@ -36,7 +36,7 @@
                                     </select>
                                 </div>
                                 <div class="form-group">
-                                    <textarea name="body"  class="form-control" id="editor" rows="6" placeholder="请填入至少三个字符">{{old('body',$topic->body)}}</textarea>
+                                    <textarea name="body"  class="form-control" id="editor" rows="6" >{{old('body',$topic->body)}}</textarea>
                                 </div>
                                 <div class="well well-sm">
                                     <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2 " aria-hidden="true"></i>提交</button>
@@ -49,3 +49,20 @@
     </div>
 
 @endsection
+@section('styles')
+    <link rel="stylesheet" href="{{asset('css/simditor.css')}}">
+@stop
+@section('scripts')
+    <script type="text/javascript" src="{{asset('js/module.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/hotkeys.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/uploader.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/simditor.js')}}"></script>
+    <script>
+        $(function(){
+            var editor = new Simditor({
+                textarea:$('#editor'),
+                placeholder:"请填写至少三个字符的内容"
+            })
+        });
+    </script>
+@stop
