ed7bf0c1a48dec1bfac75e40c2a45aaaf1b73c3f
zrr <1085530400@qq.com>
2019-11-16 23:01:24 +0800

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -4,13 +4,15 @@ namespace App\Notifications;
 
 use App\Models\Reply;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
     public $reply;
+
     /**
      * Create a new notification instance.
      *
@@ -24,32 +26,31 @@ class TopicReplied extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+        return (new MailMessage())->line('您的话题有新的回复！')
+            ->action('查看回复', $url);
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
@@ -62,17 +63,18 @@ class TopicReplied extends Notification
     public function toDatabase($notifiable)
     {
         $topic = $this->reply->topic;
-        $link = $topic->link(['#reply'.$this->reply->id]);
+        $link = $topic->link(['#reply' . $this->reply->id]);
         //存入数据库数据
         return [
-          'reply_id'=>$this->reply->id,
-          'reply_content'=>$this->reply->content,
-          'user_id'=>$this->reply->user->id,
-          'user_name'=>$this->reply->user->name,
-          'user_avatar'=>$this->reply->user->avatar,
-          'topic_link'=>$link,
-          'topic_id' =>$topic->id,
-          'topic_title'=>$topic->title
+            'reply_id' => $this->reply->id,
+            'reply_content' => $this->reply->content,
+            'user_id' => $this->reply->user->id,
+            'user_name' => $this->reply->user->name,
+            'user_avatar' => $this->reply->user->avatar,
+            'topic_link' => $link,
+            'topic_id' => $topic->id,
+            'topic_title' => $topic->title
         ];
     }
+
 }
